0b5b75e13f42db5478731837e594dcbf1e0bd9af
jefferson-kkk <jecorrea.2008@gmail.com>
2026-03-10 16:21:05 -0300

    atividade 09.03.2026

---
+++ ativdade/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: InterruptorApp()),
+  );
+}
+
+class InterruptorApp extends StatefulWidget {
+  @override
+  _InterruptorAppState createState() => _InterruptorAppState();
+}
+
+class _InterruptorAppState extends State<InterruptorApp> {
+  int estadoemocao = 0;
+
+  void altenarhumor() {
+    setState(() {
+      estadoemocao++;
+      if (estadoemocao >= 3) {
+        estadoemocao = 0;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: estadoemocao == 0
+          ? Colors.white
+          : estadoemocao == 1
+              ? Colors.green
+              : Colors.red,
+
+      appBar: AppBar(
+        title: Text(
+          'emoção',
+          style: TextStyle(
+            color: estadoemocao == 0
+                ? Colors.black
+                : Colors.white,
+          ),
+        ),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Icon(
+              estadoemocao == 0
+                  ? Icons.sentiment_neutral
+                  : estadoemocao == 1
+                      ? Icons.sentiment_satisfied
+                      : Icons.sentiment_very_dissatisfied,
+              size: 100,
+            ),
+
+            ElevatedButton(
+              onPressed: altenarhumor,
+              child: Text("Mudar emoção"),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ aula5_interruptor/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: InterruptorApp()),
+  );
+}
+
+class InterruptorApp extends StatefulWidget {
+  @override
+  _InterruptorAppState createState() => _InterruptorAppState();
+}
+
+class _InterruptorAppState extends State<InterruptorApp> {
+  bool estadoaceso = false;
+
+  void altenarluz() {
+    setState(() {
+      estadoaceso = !estadoaceso;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: estadoaceso ? Colors.black : Colors.yellow,
+
+      appBar: AppBar(
+        title: Text(
+          'interruptor',
+          style: TextStyle(color: estadoaceso ? Colors.black : Colors.yellow),
+        ),
+
+
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              estadoaceso ? Icons.lightbulb : Icons.lightbulb_outline,
+              color: estadoaceso ? Colors.yellow : Colors.black,
+              size: 100,
+            ),
+
+            // cria o
+            ElevatedButton(
+              onPressed: altenarluz,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: estadoaceso ? Colors.yellow : Colors.black,
+              ),
+              child: estadoaceso
+                  ? Text('Acender', style: TextStyle(color: Colors.black))
+                  : Text('Apagar', style: TextStyle(color: Colors.yellow)),
+            ),
+
+        
+          ],
+
+          
+        ),
+      ),
+    );
+  }
+}
